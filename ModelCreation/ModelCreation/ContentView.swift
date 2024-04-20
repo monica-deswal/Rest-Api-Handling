bca2b958e978b447a64d36d33dae3edc8d320a19
Monica deswal <89979804+Monicadeswal1@users.noreply.github.com>
2024-04-20 20:10:25 +0530

    Learning the rest APi
    Usage of
    1. nestedUnkeyedContainer
    2. nestedContainer

---
+++ ModelCreation/ModelCreation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ModelCreation
+//
+//  Created by Monica Deswal 1 on 20/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ModelCreation/ModelCreation/ModelCreationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelCreationApp.swift
+//  ModelCreation
+//
+//  Created by Monica Deswal 1 on 20/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ModelCreationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
